abcfa2f94c52f913758ba12c7b05f7e522a1a42f
Taehyuk Kang <taehyukk@gmail.com>
2022-06-16 12:27:36 -0700

    Initial version (06/16/2022)

---
+++ README.md
@@ -0,0 +1,43 @@
+Single Nucleotide Variant (SNV) and Structural Variant (SV) Analysis Pipeline
+
+The primary function of this pipeline is to analyze SNVs and SVs using the PacBio sequencing reads. This pipeline is basically consisted of the deepvariant (https://github.com/google/deepvariant) for SNVs and the pbsv (https://github.com/PacificBiosciences/pbsv) for SVs. Here, the dataset (merge) and the pbmm2 in the command-line tools of the SMRT Link v11.0 (https://www.pacb.com/support/software-downloads/) have added for building a whole process of the variant detections from preparing proper inputs. Using the interactive input functions on this pipeline, users either can choose one of the analysis types between SNV and SV or can carry out both, sequentially. Technically, the deepvariant in the SNV analysis can accept any types of sequencing outputs regardless sequencing platforms. Hence, users, who have any sequencing reads from different platforms, can solely select the deepvariant, as well.
+
+It is designed for executing in the Apollo server (apollo-acc.coh.org) of the City of Hope (Duarte, CA) using the interactive job mode (http://apollo.coh.org/user-guide/interactivejobs/). Before executing the analysis, the below modules must be loaded in advance to the server.
+
+    module load smrtlink/11.0
+    module load singularity
+
+To execute the pipeline, you need to download the python scripts of the pipeline (git clone https://github.com/tay45/PacBio_Variant_Analysis.git), first. And, copy the scripts to the folder to run the analysis (../running_folder/variantDetect.py). The folder should contain raw read (ccs.bam) and index (ccs.bam.pbi) files.
+
+And, execute the below command line in the shell.
+
+    python3 variantDetect.py
+
+The input must be HiFi reads (ccs.bam; QV>20). If the raw data is the continuous long reads (CLRs) in movieX.subreads.bam, please produce the ccs.bam via the below command line to generate the correct input.
+
+    ccs movieX.subreads.bam movieX.ccs.bam --min-rq 0.9
+    
+If the user wanted to use the SV analysis using the sequencing reads being not from the PacBio platform, an aligned bam on a reference sequence is required as the input.  
+
+During the running of the pipeline, it asks your intention of the analysis (e.g., whether or not you want to provide a tandem repeat bed file of the reference in the SV analysis) to decide the direction the next step or ask you to enter a necessary file (e.g., a genome reference) to proceed the next analysis.
+
+The 'supporting_files' folder (/net/isi-dcnl/ifs/user_data/Seq/PacBio/thkang/Pipelines/SV_SNV/supporting_files) contains a human reference genome and its tandem repeat bed files requiring for the pbsv execution as optional.
+
+    Genome reference: GRCh38.p12.genome.fa
+    Tandem_repeats_bed: human_GRCh38_no_alt_analysis_set.trf.bed
+
+Regarding the outputs;
+
+    merged.consensusreadset.xml: Merged output from different SMRT Cells.
+    *.aligned.bam: Aligned bam from pbmm2
+    *.snv.vcf: The out of deepvariant.
+    *.snv.g.vcf: The out of deepvariant (Genomic Variant Call Format).
+    log folder: logs of deepvariant.    
+    *.svsig.gz: The output of pbsv (discover).
+    *.sv.vcf: The output of pbsv (call).
+    count_sv_variant_type.txt: Counting each type of SVs.
+
+For executing this pipeline in other platforms, please satisfy the prerequisite through installing below tools.
+
+    SMRT Link v11.0 (https://www.pacb.com/support/software-downloads/)
+    deepvariant (https://github.com/google/deepvariant)
---
+++ script/variantDetect.py
@@ -0,0 +1,305 @@
+#!/usr/bin/python
+import os
+import os.path
+import subprocess
+import fileinput
+import argparse
+import sys
+import time
+import gzip
+import shutil
+
+
+#Load modules
+#module load smrtlink/11.0
+#module load singularity
+
+
+#Check the input files 
+def input_file (path):
+	if os.path.isfile(path):
+		return
+	else:
+		print("Doesn't exist. Please check the file location")
+		sys.exit()
+
+
+def merge_bams():
+	#Current_path
+	current_path = os.path.abspath(os.getcwd())
+	print(current_path)
+	time.sleep(2)
+	
+	#Argument
+	xmls = ""
+	xmls = input("Enter the paths of the dataset xmls with a space separated each: ")
+	name = ""
+	name = input("Specify explicit name for the new dataset merged: ")
+	
+	#Command line
+	merge = "dataset --log-level DEBUG --skipCounts" + " merge --remove-parentage --unique-collections --no-sub-datasets --name " + name + " merged.consensusreadset.xml " + xmls + " && " + "dataset newuuid --random merged.consensusreadset.xml"
+	os.system(merge)
+	
+	#Output
+	subprocess.call("ls merged.consensusreadset.xml", shell = True)
+	time.sleep(2)
+
+
+def align():
+	#Current_path
+	current_path = os.path.abspath(os.getcwd())
+	print(current_path)
+	print("***Aligning reads...***")
+	time.sleep(2)
+	
+	#Argument
+	global ref
+	ref = ""
+	ref = input("Enter the paths of the genome reference (.fa/fasta): ")
+	bam = ""
+	bam = input("Enter the paths of the raw bam (.bam/.xml): ")
+	global name
+	name = ""
+	name = input("Specify the sample name: ")
+	global aligned_bam
+	aligned_bam = name + ".aligned.bam"
+	
+	#Command line
+	align = "pbmm2 align " + ref + " " + bam + " " + aligned_bam + " --sort --preset CCS --sample " + name
+	os.system(align)
+	
+	#Output
+	subprocess.call("ls *.bam", shell = True)
+	time.sleep(2)
+	
+
+def pbsv():
+	#Current_path
+	current_path = os.path.abspath(os.getcwd())
+	print(current_path)
+	print("***Analyzing structural variants...***")
+	time.sleep(2)
+	
+	#Argument
+	global out
+	out = name + ".svsig.gz"
+	global sv_vcf_name 
+	sv_vcf_name = name + ".sv.vcf"
+	
+	while True:
+		answer = input("Do you have and want to provide one tandem repeat annotation .bed file of your reference? (yes/no): ")
+		if answer.lower().startswith("y"):
+			tandem = ""
+			tandem = input("Enter" + " " + """ '--tandem-repeats YOUR.annotation.bed' """ + " " + "(.bed): ")
+			#Command line (discover)
+			pbsv_discover = "pbsv discover " + tandem + " " + aligned_bam + " " + out
+			os.system(pbsv_discover)
+			#Output
+			subprocess.call("ls *.svsig.gz", shell = True)
+			time.sleep(2)
+			break
+		elif answer.lower().startswith("n"):
+			print("***Run pbsv discover without the tandem repeats annotations...***")
+			time.sleep(2)
+			#Command line (discover)
+			pbsv_discover = "pbsv discover " + aligned_bam + " " + out
+			os.system(pbsv_discover)
+			#Output
+			subprocess.call("ls *.svsig.gz", shell = True)
+			time.sleep(2)
+			break
+	
+	#Command line (call)
+	pbsv_call = "pbsv call " + ref + " " + out + " " + sv_vcf_name
+	os.system(pbsv_call)
+	
+	#Output
+	subprocess.call("ls *.sv.vcf", shell = True)
+	time.sleep(2)
+	
+	
+def count_sv_variant_types():
+	#Current_path
+	current_path = os.path.abspath(os.getcwd())
+	print(current_path)
+	print("***Counting each type of structural variants...***")
+	time.sleep(2)
+	
+	#Command line (discover)
+	count_sv = "cat" + " " + sv_vcf_name + " " + "| perl -ne" + " " + """ 'print "$1\n" if /[;\t]SVTYPE=([^;\t]+)/' """ + " " + "| sort | uniq -c > count_sv_variant_type.txt"
+	os.system(count_sv)
+	
+	#Output
+	subprocess.call("ls *.txt", shell = True)
+	time.sleep(2)
+	
+
+def deepvariant():
+	#Current_path
+	current_path = os.path.abspath(os.getcwd())
+	print(current_path)
+	print("***Analyzing single nucleotide variants...***")
+	time.sleep(2)
+	
+	#Argument
+	model_type = ""
+	model_type = input("Enter the type of the sequencing platforms (WGS,WES,PACBIO or HYBRID_PACBIO_ILLUMINA): ")
+	out_vcf = name + ".snv.vcf"
+	out_gvcf = name + ".snv.g.vcf"
+		
+	#Command line
+	deepvariant_snv = "/opt/singularity/3.7.0/bin/singularity run " + "/opt/singularity-images/deepvariant-1.3.0.simg " + "run_deepvariant " + "--model_type=" + model_type + " " +  "--ref=" + ref + " --reads=" + aligned_bam + " " + "--output_vcf=" + out_vcf + " " + "--output_gvcf=" + out_gvcf + " " + "--num_shards=16" + " " + "--logging_dir=./log" + " " + "--runtime_report" + " " + "--use_hp_information=true"
+	os.system(deepvariant_snv)
+	
+	#Output
+	subprocess.call("ls *.vcf", shell = True)
+	time.sleep(2)
+
+
+def deepvariant_2():
+	#Current_path
+	current_path = os.path.abspath(os.getcwd())
+	print(current_path)
+	print("***Analyzing single nucleotide variants...***")
+	time.sleep(2)
+	
+	#Argument
+	model_type = ""
+	model_type = input("Enter the type of the sequencing platforms (WGS,WES,PACBIO or HYBRID_PACBIO_ILLUMINA): ")
+	ref = ""
+	ref = input("Enter the paths of the genome reference (.fa/fasta): ")
+	aligned_bam = ""
+	aligned_bam = input("Enter the paths of the aligned bam (.bam): ")
+	out_vcf = aligned_bam + ".snv.vcf"
+	out_gvcf = aligned_bam + ".snv.g.vcf"
+		
+	#Command line
+	deepvariant_snv = "/opt/singularity/3.7.0/bin/singularity run " + "/opt/singularity-images/deepvariant-1.3.0.simg " + "run_deepvariant " + "--model_type=" + model_type + " " +  "--ref=" + ref + " --reads=" + aligned_bam + " " + "--output_vcf=" + out_vcf + " " + "--output_gvcf=" + out_gvcf + " " + "--num_shards=16" + " " + "--logging_dir=./log" + " " + "--runtime_report" + " " + "--use_hp_information=true"
+	os.system(deepvariant_snv)
+	
+	#Output
+	subprocess.call("ls *.vcf", shell = True)
+	time.sleep(2)
+	
+
+#Confirm the setting of requiring modules
+while True:
+	answer = input("Have you loaded the required modules before running the pipeline? (yes/no): ")
+	if answer.lower().startswith("y"):
+		print("***Start the pipeline...***")
+		time.sleep(2)
+		break
+	elif answer.lower().startswith("n"):
+		print("***Please load 'smrtlink/11.0' and 'singularity' using 'module load' in Apollo...***")
+		time.sleep(2)
+		sys.exit()
+		
+		
+#Choose the type of the variants between SNV and SV
+while True:
+	answer = input("Which type of the variants do you want to analyze? (SNV or SV): ")
+	if answer.lower().startswith("snv"):
+		print("***Start the SNV pipeline...***")
+		time.sleep(2)
+		break
+	elif answer.lower().startswith("sv"):
+		print("***Start SV the pipeline...***")
+		time.sleep(2)
+		#Whether merging SMRT Cells is necessary
+		while True:
+			answer = input("Do you have multiple raw-squencing datasets to merge? (yes/no): ")
+			if answer.lower().startswith("y"):
+				print("***Start the SV pipeline...***")
+				time.sleep(2)
+				merge_bams()
+				align()
+				pbsv()
+				count_sv_variant_types()
+				#Whether SNV analysis is necessary
+				while True:
+					answer = input("Do you want to analyze small variants as well? (yes/no): ")
+					if answer.lower().startswith("y"):
+						print("***Start the SNV pipeline...***")
+						time.sleep(2)
+						deepvariant()
+						print("***Completing the process...***")
+						time.sleep(2)
+						sys.exit()
+					elif answer.lower().startswith("n"):
+						print("***Completing the process...***")
+						time.sleep(2)
+						sys.exit()
+			elif answer.lower().startswith("n"):
+				print("***Start the SV pipeline...***")
+				time.sleep(2)
+				align()
+				pbsv()
+				count_sv_variant_types()
+				while True:
+					answer = input("Do you want to analyze small variants as well? (yes/no): ")
+					if answer.lower().startswith("y"):
+						print("***Start the SNV pipeline...***")
+						time.sleep(2)
+						deepvariant()
+						print("***Completing the process...***")
+						time.sleep(2)
+						sys.exit()
+					elif answer.lower().startswith("n"):
+						print("***Completing the process...***")
+						time.sleep(2)
+						sys.exit()
+		
+	
+#Whether merging SMRT Cells is necessary
+while True:
+	answer = input("Is this analysis for the PacBio sequencing reads? (yes/no): ")
+	if answer.lower().startswith("y"):
+		print("***Start the SNV pipeline...***")
+		time.sleep(2)
+		while True:
+			answer = input("Do you have multiple raw-squencing datasets to merge? (yes/no): ")
+			if answer.lower().startswith("y"):
+				merge_bams()
+				align()
+				deepvariant()
+				#Whether SV analysis is necessary
+				while True:
+					answer = input("Do you want to analyze structual variants as well? (yes/no): ")
+					if answer.lower().startswith("y"):
+						print("***Start the SV pipeline...***")
+						time.sleep(2)
+						pbsv()
+						count_sv_variant_types()
+						print("***Completing the process...***")
+						time.sleep(2)
+						sys.exit()
+					elif answer.lower().startswith("n"):
+						print("***Completing the process...***")
+						time.sleep(2)
+						sys.exit()
+			elif answer.lower().startswith("n"):
+				print("***Start the SNV pipeline...***")
+				time.sleep(2)
+				align()
+				deepvariant()
+				while True:
+					answer = input("Do you want to analyze structual variants as well? (yes/no): ")
+					if answer.lower().startswith("y"):
+						print("***Start the SV pipeline...***")
+						time.sleep(2)
+						pbsv()
+						count_sv_variant_types()
+						print("***Completing the process...***")
+						time.sleep(2)
+						sys.exit()
+					elif answer.lower().startswith("n"):
+						print("***Completing the process...***")
+						time.sleep(2)
+						sys.exit()	
+	elif answer.lower().startswith("n"):
+		print("***Start the SNV pipeline...***")
+		time.sleep(2)
+		deepvariant_2()
+		print("***Completing the process...***")
+		time.sleep(2)
+		sys.exit()
